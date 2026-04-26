80d82a3da0164ef53bf23d5a6dbdda0c32724442
afamadocomprador <165040121+afamadocomprador@users.noreply.github.com>
2026-04-26 21:31:46 +0200

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -10,6 +10,21 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            { key: 'X-Content-Type-Options', value: 'nosniff' },
+            { key: 'X-Frame-Options', value: 'DENY' },
+            { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+            { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+            { key: 'Content-Security-Policy', value: "upgrade-insecure-requests" }
+          ],
+        },
+      ];
+    },
 };
 
 export default nextConfig;
