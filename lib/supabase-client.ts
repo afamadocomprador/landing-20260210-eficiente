90f252c5e6021ab4eca0d121f653d435674d4967
afamadocomprador <165040121+afamadocomprador@users.noreply.github.com>
2026-05-03 09:22:43 +0200

    Add files via upload

---
+++ lib/supabase-client.ts
@@ -0,0 +1,16 @@
+// lib/supabase-client.ts
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Esta variable vive fuera del ciclo de vida de React
+let supabaseInstance: any = null;
+
+export const getSupabaseClient = () => {
+  if (!supabaseInstance) {
+    supabaseInstance = createClient(supabaseUrl, supabaseAnonKey);
+  }
+  return supabaseInstance;
+};
